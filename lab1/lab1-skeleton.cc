a2016387b7009b05460b99bb8b4d1917eedd577a
Kat <myun7@illinois.edu>
2019-01-22 11:39:48 -0600

    lab1 done

--- lab1/lab1-skeleton.cc
+++ lab1/lab1-skeleton.cc
@@ -10,10 +10,27 @@ Abstract: Skeleton for Lab 1
 #include "image.h"
 using namespace std;
 
+int setAverage(Image& image, int row, int col){
+	int sum = 0;
+	for(int i = row; i<row+4; i++){
+		for(int j = col; j<col+4;j++){
+			sum+=image.Pixel(i,j);
+		}
+	}
+	int average = sum/16;
+
+	for(int i = row; i<row+4; i++){
+		for(int j = col; j<col+4;j++){
+			image.Pixel(i,j) = average;
+		}
+	}
+}
+
+
 int main (int argc, char* argv[])
 {
 	// check parameters' correctness
-	if (argc != 3) 
+	if (argc != 3)
 	{
 		cerr << "Usage: lab1 in.png out.png" << endl;
 		return 1;
@@ -26,11 +43,20 @@ int main (int argc, char* argv[])
 	/*==
 
 	Insert Lab 1 Code Here
-	
+
 	==*/
-	
+
+	for(int i = 0; i<image.Height()-4;i=i+4){
+		for(int j = 0; j<image.Width()-4;j=j+4){
+			setAverage(image,i,j);
+			printf("%d %d\n",i,j);
+		}
+	}
+
+
+
 	// save it
 	image.SavePng (argv[2]);
-	
+
 	return 0;
 }
